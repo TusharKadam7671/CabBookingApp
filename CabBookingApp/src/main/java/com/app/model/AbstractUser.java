61c9dcba8666e1b683c93dd5f1fc84615f5e534a
TusharKadam7671 <kadamtushar080@gmail.com>
2023-01-05 13:32:47 +0530

    Cab Booking Application

--- CabBookingApp/src/main/java/com/app/model/AbstractUser.java
+++ CabBookingApp/src/main/java/com/app/model/AbstractUser.java
@@ -2,13 +2,17 @@ package com.app.model;
 
 import com.app.Enum.ERole;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
 
 @MappedSuperclass
+
 public abstract class AbstractUser {
+	
 	
 	private String username;
 	private String password;
@@ -21,5 +25,78 @@ public abstract class AbstractUser {
 	
 	@Enumerated(EnumType.STRING)
 	private ERole role;
+	
+	
+	
 
+	public AbstractUser() {
+		super();
+	}
+
+	public AbstractUser(String username, String password, Address address, String mobileNumber, String email,
+			ERole role) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.address = address;
+		this.mobileNumber = mobileNumber;
+		this.email = email;
+		this.role = role;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	public String getMobileNumber() {
+		return mobileNumber;
+	}
+
+	public void setMobileNumber(String mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public ERole getRole() {
+		return role;
+	}
+
+	public void setRole(ERole role) {
+		this.role = role;
+	}
+
+	@Override
+	public String toString() {
+		return "AbstractUser [username=" + username + ", password=" + password + ", address=" + address
+				+ ", mobileNumber=" + mobileNumber + ", email=" + email + ", role=" + role + "]";
+	}
+
+	
 }
--- CabBookingApp/src/main/java/com/app/model/Address.java
+++ CabBookingApp/src/main/java/com/app/model/Address.java
@@ -1,16 +1,45 @@
 package com.app.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
+import lombok.ToString;
 
 @Data
+@ToString
 public class Address {
 	
 	
-	private Integer AddressId;
+//	@GeneratedValue(strategy = GenerationType.AUTO)
+//	private Integer addressId;
+	
 	private String addressLine1;
 	private String addressLine2;
 	private String city;
 	private String country;
 	private String pincode;
+	
+	
+	public Address(String addressLine1, String addressLine2, String city, String country,
+			String pincode) {
+		super();
+		this.addressLine1 = addressLine1;
+		this.addressLine2 = addressLine2;
+		this.city = city;
+		this.country = country;
+		this.pincode = pincode;
+	}
+
+
+	public Address() {
+		super();
+	}
+
+
+
+
+	
 
 }
